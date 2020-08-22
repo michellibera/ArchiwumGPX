1a043fae31f18e62315f1742bf4b7c8d87eda99f
Zefffir <michel.libera@gmail.com>
2020-08-22 15:56:28 +0200

    Delete staticfilesdirs from settings

--- gpstrack/settings.py
+++ gpstrack/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'statics'),)
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'statics'),)
 
 LOGIN_REDIRECT_URL = 'profile'
 LOGIN_URL = 'login'
